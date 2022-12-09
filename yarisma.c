3c62b45e1bcc208b98ec28e2f3a8f0194db22063
FURKAN EMİR KAYA <furkanemirkaya1905@gmail.com>
2022-12-09 17:34:23 +0300

    Artik program calisirken terminal Unicode destekliyor.

--- yarisma.c
+++ yarisma.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+#include <Windows.h>
         
 int main(){
+    SetConsoleOutputCP(65001);
+    setlocale(LC_ALL, ".utf8");
 
 
 }

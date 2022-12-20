dd4349fb2c229794e785b7d6810369e46ca5e276
Furkan Emir <furkanemirkaya@hotmail.com>
2022-12-20 21:44:04 +0300

    Add files via upload

--- kim-milyoner-olmak-ister-yarisma.c
+++ kim-milyoner-olmak-ister-yarisma.c
@@ -9,12 +9,13 @@ Hasan Arif Doğan 22181617016 , Erdem Tarık Güral 22181617002 , Furkan Emir KA
 #include <Windows.h> //Terminalde Unicode yazı gösterebilmek için
 #include <conio.h> //getch
 #include <ctype.h> //toupper
-#define MAX_SATIR 2048
+#define MAX_KARAKTER 2048 //txt DOSYASINDAN BİR SATIRDA MAX KAÇ KARAKTER OKUNACAĞI 
 #define CVP_ANAHTAR_U 26 //CEVAP ANAHTARI DİZİLERİNİN UZUNLUĞU
 
 int sayac=1; //YARIŞMACININ KAÇINCI SORUDA OLDUĞUNU TAKİP EDECEK SAYAÇ
-
 int rdm=0; //RANDOM DEĞERİN TUTULUP/ATANACAĞI DEĞİŞKEN (ÇEKİLEN SORUNUN SATIR NUMARASI BU DEĞŞİKENDE YANİ CEVAP DİZİSİNİN İNDİSİ OLARAK BUNU KULLANDIM)
+
+char tekrar; //Yarışmayı tekrarlama değişkeni
 
 int k=0; //Yarı yarıya jokeri kontrolü yy
 int l=0; //iki cevap jokeri kontrolü ic
@@ -23,17 +24,17 @@ int m=0; //Yeni soru jokeri kontrolü ys
 char isim[16]; //OYUNU OYNAYAN KULLANICININ ADI
 char cevap[3]; //KULLANICININ SORUYA GİRDİĞİ CEVAP (ic,ys,yy,a,b,d,c) girilebilir
 char kolayCVPA[CVP_ANAHTAR_U]={'D','A','A','A','B','C','B','A','A','D','D','B','A','D','B','B','B','A','D','C','A','C','B','C','C'}; //1-3 ARASI ÇEKİLECEK SORULARIN CEVAPLARI
-char ortaCVPA[CVP_ANAHTAR_U]={'A','B','C','B','A','D','B','A','D','A','A','D','D','B','C','A','A','B','D','A','A','C','B','C','A'}; //3-7 ARASI ÇEKİLECEK SORULARIN CEVAPLARI
+char ortaCVPA[CVP_ANAHTAR_U]={'A','B','C','B','A','D','B','A','D','A','A','D','D','B','C','A','A','B','D','A','B','C','B','C','A'}; //3-7 ARASI ÇEKİLECEK SORULARIN CEVAPLARI
 char zorCVPA[CVP_ANAHTAR_U]={'C','B','C','D','B','C','A','C','D','C','D','A','C','B','A','D','C','A','C','A','C','C','B','D','C'}; //7-10 ARASI ÇEKİLECEK SORULARIN CEVAPLARI
 
 //---FONKSİYONLARIN PROTOTİPLERİ/AÇIKLAMASI---
-void tanitim(void);   //   <<
-void cevapAl_Byap(void);   //KULLANICI CEVAp ALIP BÜYÜK HARFE DÖNDÜRÜR   <<
+void tanitim(void);   //KULLANICIYA OYUNUN KURALLARINI ANLATAN GİRİŞ EKRANI   <<
+void cevapAl_Byap(void);   //KULLANICI CEVAP ALIP BÜYÜK HARFE DÖNDÜRÜR   <<
+void tekrarOyna(void); //KULLANICININ OYUNU TEKRAR OYNAMASINI SAĞLAYAN FONKSİYON <<
 
 void yariYariJ(void); //YARI YARIYA JOKERİ  
 void ikiCevapJ(void); //İKİ CEVAP JOKERİ -BU JOKER 7. SORU DOĞRU BİLİNDİYSE KULLANILABİLİR-   <<
 void yeniSoruJ(void); //YENİ SORU JOKERİ (AYNI ZORLUKTA YENİ SORU GETİRİR)   <<
-
 
 void yonlendir(void); //RANDOM SAYIYI OLUŞTURUR VE SAYACA GÖRE AKIŞI kRandSoru()/oRandSoru()/zRandSoru()' YA GÖNDERİR    <<
 
@@ -57,8 +58,6 @@ int main(){
     setlocale(LC_ALL, ".utf8");
     srand(time(NULL));
 
-    char tekrar; //Yarışmayı tekrarlama değişkeni
-
     do{ //Oyunu tekrar oynamamıza yarayan do while döngüsü
         int para=0; //YARIŞMACININ KAZANDIĞI PARA MİKTARI
         int *pp=&para; //Para değişkenin adresini tutan pointer
@@ -80,41 +79,32 @@ int main(){
                     do{ //Çekilmek isteme sırasında farklı bir veri girildiğinde tekrardan soran do while döngüsü 
                     cevapAl_Byap();
                     } while(cevap[0] != 'E' && cevap[0] != 'H');
-                        if(cevap[0] == 'E'){
+                        if(cevap[0] == 'E'){ 
+                            tekrarOyna();
                             break; 
-                        system("cls");
-                        continue;
-                    }          
+                        }    
+                    system("cls");
+                    continue;
+                              
                 }   
             }
             else if(x==0){
-            printf("Sorun yanlış.Kaybettin\nMalesef %d ₺ ile seni yolluyoruz.\n",para/2);
-            break; 
+                printf("Sorun yanlış.Kaybettin\nMalesef %d ₺ ile seni yolluyoruz.\n\n",para/2);
+                tekrarOyna();
+                break; 
             }        
-        } while (sayac!=11 && x!=0);
+        } while (sayac!=11);
     
         if(sayac==11){
             printf("Tebrikler! 10. soruyu cevaplayarak 1.000.000 ₺'nin sahibi oldunuz!\n\n\a\a\a\a");
-            printf("Tekrar oynamak ister misiniz? Evet(E) veya Hayır(H) tuşlayınız.\t\n\n");
-            tekrar=getch();
-            tekrar=toupper(tekrar);
-            sayac=1, k=0, l=0, m=0;
+            tekrarOyna();
         }    
-        else if(cevap[0]=='E'){
-            printf("Yarışmadan %d ₺ ile çekildin\n",para);
-        }
-        else if(x==0){
-            printf("Tekrar oynamak ister misiniz? Evet(E) veya Hayır(H) tuşlayınız.\t\n\n");
-            tekrar=getch();
-            tekrar=toupper(tekrar);
-            sayac=1, k=0, l=0, m=0;
-        }
     } while (tekrar == 'E');
     system("pause");
     return 0;
 }
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<fONKSİYONLATRIN TANIMLAMALARI>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<fONKSİYONLARIN TANIMLAMALARI>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 void tanitim(){
     printf("\nMerhaba! Kim Milyoner Olmak İster Yarışmasına Hoşgeldin!\nSeni daha iyi tanıyabilmemiz için lütfen adını gir: ");   
     gets(isim);
@@ -130,9 +120,17 @@ void tanitim(){
 
 void cevapAl_Byap(){
     gets(cevap);
-    for(int i=0;i<3;i++){    
+    for(int i=0;i<2;i++){    
         cevap[i]=toupper(cevap[i]);
     }
+}
+
+
+void tekrarOyna(){
+    printf("Tekrar oynamak ister misiniz? Evet(E) veya Hayır(H) tuşlayınız.\t\n\n");
+    tekrar=getch();
+    tekrar=toupper(tekrar);
+    sayac=1, k=0, l=0, m=0;
 }
 //-------------------------------------------------------------------------------------JOKER FONKSİYONLARI---------------------------------------------------------------------------------------
 void yariYariJ(){
@@ -255,7 +253,7 @@ void yonlendir(){
 void kRandSoru(){
     
     FILE *dosya;
-    char gecici[MAX_SATIR];
+    char gecici[MAX_KARAKTER];
 
     dosya=fopen("Kolay_sorular.txt","r");
 
@@ -266,11 +264,11 @@ void kRandSoru(){
     }
 
     bool oku_devam = true;
-    int okunan_satır = 1;
+    int okunan_satır = 1; //fgets'i kaç kere çalıştırdığımızı tutar
 
     do
     {
-        fgets(gecici,MAX_SATIR,dosya);
+        fgets(gecici,MAX_KARAKTER,dosya);
         if(feof(dosya)) //feof() dosya sonuna geldiğinde 0 döndürür
         {
             oku_devam=false;
@@ -279,7 +277,7 @@ void kRandSoru(){
         {
             oku_devam=false;
             
-            printf("%s\n",gecici);
+            printf("%s\n",gecici);  
         }
 
         okunan_satır++;
@@ -292,7 +290,7 @@ void kRandSoru(){
 void kSiklar(){
 
     FILE *dosya;
-    char gecici[MAX_SATIR];
+    char gecici[MAX_KARAKTER];
 
     dosya=fopen("Kolay_cevaplar.txt","r");
 
@@ -307,7 +305,7 @@ void kSiklar(){
 
     do
     {
-        fgets(gecici,MAX_SATIR,dosya);
+        fgets(gecici,MAX_KARAKTER,dosya);
         if(feof(dosya)) //feof() dosya sonuna geldiğinde 0 döndürür
         {
             oku_devam=false;
@@ -328,7 +326,7 @@ void kSiklar(){
 void oRandSoru(){
 
     FILE *dosya;
-    char gecici[MAX_SATIR];
+    char gecici[MAX_KARAKTER];
 
     dosya=fopen("Orta_sorular.txt","r");
 
@@ -343,7 +341,7 @@ void oRandSoru(){
 
     do
     {
-        fgets(gecici,MAX_SATIR,dosya);
+        fgets(gecici,MAX_KARAKTER,dosya);
         if(feof(dosya)) //feof() dosya sonuna geldiğinde 0 döndürür
         {
             oku_devam=false;
@@ -364,7 +362,7 @@ void oRandSoru(){
 void oSiklar(){
 
     FILE *dosya;
-    char gecici[MAX_SATIR];
+    char gecici[MAX_KARAKTER];
 
     dosya=fopen("Orta_cevaplar.txt","r");
 
@@ -379,7 +377,7 @@ void oSiklar(){
 
     do
     {
-        fgets(gecici,MAX_SATIR,dosya);
+        fgets(gecici,MAX_KARAKTER,dosya);
         if(feof(dosya)) //feof() dosya sonuna geldiğinde 0 döndürür
         {
             oku_devam=false;
@@ -400,7 +398,7 @@ void oSiklar(){
 void zRandSoru(){
 
     FILE *dosya;
-    char gecici[MAX_SATIR];
+    char gecici[MAX_KARAKTER];
 
     dosya=fopen("Zor_sorular.txt","r");
 
@@ -415,7 +413,7 @@ void zRandSoru(){
 
     do
     {
-        fgets(gecici,MAX_SATIR,dosya);
+        fgets(gecici,MAX_KARAKTER,dosya);
         if(feof(dosya)) //feof() dosya sonuna geldiğinde 0 döndürür
         {
             oku_devam=false;
@@ -436,7 +434,7 @@ void zRandSoru(){
 void zSiklar(){
 
     FILE *dosya;
-    char gecici[MAX_SATIR];
+    char gecici[MAX_KARAKTER];
 
     dosya=fopen("Zor_cevaplar.txt","r");
 
@@ -451,7 +449,7 @@ void zSiklar(){
 
     do
     {
-        fgets(gecici,MAX_SATIR,dosya);
+        fgets(gecici,MAX_KARAKTER,dosya);
         if(feof(dosya)) //feof() dosya sonuna geldiğinde 0 olmayan bir değer  döndürür
         {
             oku_devam=false;
